74b4c2a56733df1ff90a86c8978ae3e97ebf7355
shibasab <34082145+shibasab@users.noreply.github.com>
2019-06-20 22:20:11 +0900

    Add account activation

--- db/migrate/20190619164019_add_admin_to_users.rb
+++ db/migrate/20190619164019_add_admin_to_users.rb
@@ -1,5 +1,7 @@
 class AddAdminToUsers < ActiveRecord::Migration[5.2]
     def change
+        add_column :users. :activation_digest, :string
         add_column :users, :admin, :boolean, default: false
+        add_column :users, :activated_at, :datatime
     end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
         @user = users(:michael)
     end
 
+    test "login with invalid information" do
+        get login_path
+        assert_template 'sessions/new'
+        post login_path, params: { session: { email: "", password: "" } }
+        assert_template 'sessions/new'
+        assert_not flash.empty?
+        get root_path
+        assert flash.empty?
+    end
 
     test "login with valid information followed by logout" do
         get login_path
@@ -18,6 +27,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
         assert_select "a[href=?]", login_path, count: 0
         assert_select "a[href=?]", logout_path
         assert_select "a[href=?]", user_path(@user)
+        
         delete logout_path
         assert_not is_logged_in?
         assert_redirected_to root_url
@@ -31,7 +41,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     test "login with remembering" do
         log_in_as(@user, remember_me: '1')
-        assert_equal cookies['remember_token'], assigns(:user).remember_token
+        assert_not_empty cookies['remember_token']
     end
 
     test "login without remembering" do
