d8ba3d44be82bb12bd481e1566eeb382a4af3c99
shibasab <34082145+shibasab@users.noreply.github.com>
2019-06-25 12:52:04 +0900

    del mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,10 +31,13 @@ Rails.application.configure do
     config.active_storage.service = :local
 
     # Don't care if the mailer can't send.
-    config.action_mailer.raise_delivery_errors = false
-    config.action_mailer.delivery_method = :test
+    config.action_mailer.raise_delivery_errors = true
+    #config.action_mailer.delivery_method = :test
     host = 'localhost:3000' # ここは自分のテスト環境を使うi
-    config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+    #config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {:address => 'localhost', port: 1025 }
     config.action_mailer.perform_caching = false
 
     # Print deprecation notices to the Rails logger.
